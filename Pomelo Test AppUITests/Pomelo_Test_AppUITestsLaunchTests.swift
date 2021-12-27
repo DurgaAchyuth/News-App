1d0806c847c9b6464dabdfc4b796c29cfdb1cdfc
Durga Achyuth <achyuth4365@gmail.com>
2021-12-27 19:44:27 +0530

    Initial Commit

---
+++ Pomelo Test AppUITests/Pomelo_Test_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Pomelo_Test_AppUITestsLaunchTests.swift
+//  Pomelo Test AppUITests
+//
+//  Created by Achyuth Bujjigadu  on 27/12/21.
+//
+
+import XCTest
+
+class Pomelo_Test_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
